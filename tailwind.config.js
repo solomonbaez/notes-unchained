8ae2ee6fd9a2df2386b0dfcd040f74494c778108
solomonbaez <solomon.james.baez@gmail.com>
2023-07-27 23:18:57 -0700

    Polished up the notes_list frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./notes/templates/notes/**/*.{py,html}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
